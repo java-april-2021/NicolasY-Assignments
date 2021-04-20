d3272408463d7ce64afeb56613355c3e118c3dcb
Nick Yeon <nkyeon0709@gmail.com>
2021-04-19 18:48:47 -0700

    Completed Assignment

---
+++ 02-java-oop/03-zoo-keeper-pt1/Gorilla.java
@@ -0,0 +1,21 @@
+public class Gorilla extends Mammal {
+    public Gorilla(int energyLevel){
+        super(energyLevel);
+    }
+    
+    public void throwSomething(){
+        System.out.println("Threw something over there");
+        energyLevel -= 5;
+    }
+
+    public void eatBananas(){
+        System.out.println("Ate a banana");
+        energyLevel += 10;
+    }
+
+    public void climb(){
+        System.out.println("Climing something");
+        energyLevel -= 10;
+    }
+
+}
---
+++ 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
@@ -0,0 +1,15 @@
+class GorillaTest {
+    public static void main(String[] args){
+        Gorilla Donkey = new Gorilla(100);
+        Donkey.displayEnergy();
+        Donkey.throwSomething();
+        Donkey.throwSomething();
+        Donkey.throwSomething();
+        Donkey.displayEnergy();
+        Donkey.eatBananas();
+        Donkey.eatBananas();
+        Donkey.displayEnergy();
+        Donkey.climb();
+        Donkey.displayEnergy();
+    }
+}
